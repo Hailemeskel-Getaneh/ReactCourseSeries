32c2049afdf86ea990ee5f5e11437cc13466a213
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-01-16 21:48:05 +0300

    conditional rendering

---
+++ 11ConditionalRendering/src/components/IfElseRendering.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+ class IfElseRendering extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         isLogedIn : false
+      }
+    }
+
+
+    
+  render() {
+    if(this.state.isLogedIn){
+      return <h3>Welcom Haile</h3>
+  }
+  else{
+    return <h3>Welcome Guest</h3>
+  }
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default IfElseRendering
---
+++ 11ConditionalRendering/src/note.md
@@ -0,0 +1,7 @@
+There are four ways of condional rendering
+
+1. using if else statements
+    - can not be written in the JSX rather in the render or outside the jsx return
+2. Element variables
+3. Ternary condtional operators
+4. short circuit operators
